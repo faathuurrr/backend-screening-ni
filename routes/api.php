675048ab8a839a1e67052515cf2ebad607948cf1
faathuurrr <fathurrahmansyah03@gmail.com>
2024-01-01 00:04:24 +0700

    clearing route

--- routes/api.php
+++ routes/api.php
@@ -30,17 +30,3 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 Route::post('/logout', App\Http\Controllers\Api\LogoutController::class)->name('logout');
 
 Route::apiResource('/user', 'App\Http\Controllers\Api\UserController');
-
-
-    // User Group
-    // Route::prefix('/user')->name('user.')->middleware('auth:sanctum')->group(function () {
-    //     Route::get('/', [UserController::class, 'get'])->name('get');
-    //     Route::post('/', [UserController::class, 'create'])->name('create');
-    //     Route::put('/', [UserController::class, 'update'])->name('update');
-    //     Route::delete('/', [UserController::class, 'delete'])->name('delete');
-    // });
-
-
-// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-//     return $request->user();
-// });
